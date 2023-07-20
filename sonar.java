77a9b302dabef3a79b5ce43e8370dc9d80262b3f
~SG~ <gseetharaman86@gmail.com>
2023-07-20 09:01:20 +0530

    Update sonar.java

--- sonar.java
+++ sonar.java
@@ -2,5 +2,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello SonarQube!");
 		String unread = "Let's do something evil.";
+		System.out.println("Hello SonarQube!");
+		System.out.println("Hello SonarQube! Additional Coverage");
 	}
 }
